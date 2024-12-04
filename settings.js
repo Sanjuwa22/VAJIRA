fb0e295b5fbc9843ddc945f4f49a0d238b05511a
Mr.Sanjula Samarasinghe <96864439+Sanjuwa22@users.noreply.github.com>
2024-12-04 21:26:04 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+global.email = 'henz@gmail.com'
+global.github = 'https://github.com/Henhen321'
+global.location = 'Indonesia'
+global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
+global.author = 'C-Arv';
+global.mess = {
+    banned: '`You are *Banned* fron using commands!`',
+    bangc: '`This Group is *Banned* from using Commands!`',
+    success: '`✓ Success`',
+    admin: '`This feature is only for group admins`',
+    botAdmin: '`I am not an admin!`',
+    owner: '`You are not my owner`',
+    group: '`You can use this command only in groups ❌`',
+    private: '`Feature is used only for private chats!`',
+    bot: '`Bot number user special features`',
+    wait: '*Processing Your request*',
+    endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
+}
+
+global.owner = [process.env.OWNER_NUMBER] || ['6285866034212'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+
+module.exports = {    
+SESSION_ID: process.env.SESSION_ID || 'youre session id',
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
+PREFIX:  process.env.PREFIX || ['.'] ,
+LANG:  process.env.LANG || 'SI' ,    
+ANTI_BAD: process.env.ANTI_BAD || false  ,
+AUTO_REACT:  process.env.AUTO_REACT  || true  ,    
+AUTO_TYPING:  process.env.AUTO_TYPING  || true  ,
+AUTO_RECORDING:  process.env.AUTO_RECORDING  || true  ,
+AUTO_READ:  process.env.AUTO_READ  || true  ,
+AUTO_BIO:  process.env.AUTO_BIO  || true  ,    
+ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,            
+WORK_TYPE: process.env.WORK_TYPE || 'private'  ,
+ANTI_LINK: process.env.ANTI_LINK || false  ,
+ANTI_BOT: process.env.ANTI_BOT || false  ,
+OWNER_NUMBER: process.env.OWNER_NUMBER || '94719199757'  ,    
+ALIVE: process.env.ALIVE || `default`,
+OWNER_NAME: process.env.OWNER_NAME || './Henz' ,     
+AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true  ,    
+AUTO_VOICE:  process.env.AUTO_VOICE  || true  ,
+AUTO_STICKER: process.env.AUTO_STICKER || true  ,
+WELCOME:  process.env.WELCOME  || true ,    
+NUMBER_212_BLOCK: process.env.NUMBER_212_BLOCK || true  ,
+FOOTER: process.env.FOOTER ||  '© - C-Arv ＭＤ',
+LOGO: process.env.LOGO || `https://i.ibb.co/pjyr56L/image.jpg` ,
+IMAGE: process.env.IMAGE || `https://i.ibb.co/pjyr56L/image.jpg`     
+};
